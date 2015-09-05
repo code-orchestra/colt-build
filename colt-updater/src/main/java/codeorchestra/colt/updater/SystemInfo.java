9799b19ba78a46509deaa864c9729a6468020ad7
SlavaRa <ratdg@ya.ru>
2015-09-05 22:48:59 +0300

    gradle build

--- colt-updater/src/main/java/codeorchestra/colt/updater/SystemInfo.java
+++ colt-updater/src/main/java/codeorchestra/colt/updater/SystemInfo.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package codeorchestra.colt.updater;
+package codeOrchestra.colt.updater;
 
 @SuppressWarnings({"HardCodedStringLiteral", "UtilityClassWithoutPrivateConstructor"})
 public class SystemInfo {
